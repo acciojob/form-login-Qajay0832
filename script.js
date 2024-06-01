07633cd3ac5ce59c886059db959964afc75a0cd7
Qajay0832 <123375564+Qajay0832@users.noreply.github.com>
2024-06-01 15:20:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
+	event.preventDefault()
+	const fName = document.getElementsByName("fname")[0];
+	const lName = document.getElementsByName("lname")[0];
+	
+	
     //Write your code here
+	alert(fName.value+" "+lName.value)
 
 }
